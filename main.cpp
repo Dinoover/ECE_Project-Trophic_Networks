c787d2a92d7b93403b62e92dfb4beaa71a6b92e5
Mikhali <33858877+Mikhali@users.noreply.github.com>
2018-04-04 22:26:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,44 +3,63 @@
 
 #include "graph.h"
 
+
+
 int main()
 {
     /// A appeler en 1er avant d'instancier des objets graphiques etc...
     grman::init();
 
-    /// Le nom du répertoire où se trouvent les images à charger
+    /// Le nom du rÃ©pertoire oÃ¹ se trouvent les images Ã  charger
     grman::set_pictures_path("pics");
 
-    Graph clown, donjon, g;
-    int a;
+    Graph clown, donjon, classique, g;
+
+    std::string a;
 
     clown.load(1);
     donjon.load(2);
+    classique.load(3);
 
-    std::cout<<"select a graph or say 0 to end process"<<std::endl;
+    std::cout<< std::endl <<"attention, si vous revenez sur un graphe, vous reviendrez sur les poids au lancement du programme"<<std::endl;
+    std::cout<<"choisissez un graphe ou appuyez sur 0 pour arreter le processus"<<std::endl;
     std::cin >> a;
 
-    while(a!=0)
+    while(a!="0")
     {
-        switch(a)
+        if(a.compare("classique")==0&&g.m_name!="classique")
         {
-            case 1: g = clown; break;
-            case 2: g = donjon; break;
-        };
+            g=classique;
+        }else if(a.compare("clown")==0&&g.m_name!="clown")
+        {
+            g=clown;
+        }else if(a.compare("donjon")==0&&g.m_name!="donjon")
+        {
+            g=donjon;
+        }else if(a.compare("new")==0)
+        {
+           g.new_sommet();
+        }else if(a.compare("delete")==0)
+        {
+            g.delete_sommet();
+        }else if(a.compare("kconexe")==0)
+        {
+            g.kconexe(g);
+        }
 
 
         /// Vous gardez la main sur la "boucle de jeu"
-        /// ( contrairement à des frameworks plus avancés )
+        /// ( contrairement Ã  des frameworks plus avancÃ©s )
         while (!key[KEY_ESC])
         {
-            /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
+            /// Il faut appeler les mÃ©thodes d'update des objets qui comportent des widgets
             g.update();
 
-            /// Mise à jour générale (clavier/souris/buffer etc...)
+            /// Mise Ã  jour gÃ©nÃ©rale (clavier/souris/buffer etc...)
             grman::mettre_a_jour();
         }
         g.save();
-        std::cout<<"select a graph or say 0 to end process"<<std::endl;
+        std::cout<<"choisissez un graphe ou appuyez sur 0 pour arreter le processus"<<std::endl;
         std::cin>>a;
     }
 
@@ -50,5 +69,6 @@ int main()
     return 0;
 }
 END_OF_MAIN();
+
 
 
