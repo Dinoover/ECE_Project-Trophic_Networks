1383cd5ed0462cd3075386ab534f7ea9dc2471ec
Miletic Petar <miletic.petar.mp@gmail.com>
2018-04-06 15:31:30 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "grman/grman.h"
+#include <iostream>
+
+#include "graph.h"
+
+int main()
+{
+    /// A appeler en 1er avant d'instancier des objets graphiques etc...
+    grman::init();
+
+    /// Le nom du répertoire où se trouvent les images à charger
+    grman::set_pictures_path("pics");
+
+    /// Un exemple de graphe
+    Graph g;
+    g.make_example();
+
+    int exit=0;
+
+    /// Vous gardez la main sur la "boucle de jeu"
+    /// ( contrairement à des frameworks plus avancés )
+    while (exit!=1)
+    {
+        /// Il faut appeler les méthodes d'update des objets qui comportent des widgets
+        g.update(&exit);
+
+        /// Mise à jour générale (clavier/souris/buffer etc...)
+        grman::mettre_a_jour();
+    }
+
+    grman::fermer_allegro();
+
+    return 0;
+}
+END_OF_MAIN();
+
+
